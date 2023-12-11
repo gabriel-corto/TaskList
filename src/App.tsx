aeb63b1caff20ce058a4b1547a13311a75d053bf
gabriel-corto <digital.gabriel2006@gmail.com>
2023-12-11 14:50:23 +0100

    Add Event to Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 
+import { FormEvent } from "react";
 import Form from "./components/Form";
 import TaskList from "./components/TaskList";
 
 export default function App() {
+
+  function handleSaveTask(event: FormEvent) {
+    event.preventDefault()
+    console.log("Save")
+  }
   return (
     <div className="app">
       <header>
         <h1> Tasks </h1>
       </header>
 
-      <Form />
-      
+      <Form onSaveTask={handleSaveTask} />
+
       <TaskList />
     </div>
   )
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,16 +3,20 @@ import Input from './Input'
 import { Check } from 'phosphor-react'
 
 import styles from './Form.module.css'
+import { FormEventHandler } from 'react'
 
-export default function Form() {
+type formProps = {
+  onSaveTask: FormEventHandler
+}
+
+export default function Form({ onSaveTask }: formProps) {
   return (
-    <form className={styles.form}>
+    <form action='#' className={styles.form} onSubmit={onSaveTask}>
       <Input 
         Name='task'
         Type='text'
         PlaceHolder='Add new task...'
       />
-
       <button> <Check weight='bold' /> Add</button>
     </form>
   )
