9e45d46e01a5a7af54d1d9cbf4668d068d35cca2
gabriel-corto <digital.gabriel2006@gmail.com>
2023-12-14 13:35:34 +0100

    Update to DepLoy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { ChangeEvent, EventHandler, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 
 import Form from "./components/Form";
 import TaskList from "./components/TaskList";
